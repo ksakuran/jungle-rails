77b20945f4c78c9f95af416496062059918421a0
Kat Nishimura <katherine.nishimura@live.ca>
2023-03-08 03:14:17 +0000

    fix ui for empty cart, add bootstrap for alert

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,11 +7,13 @@ class ApplicationController < ActionController::Base
   private
 
   def cart
+    
     @cart ||= cookies[:cart].present? ? JSON.parse(cookies[:cart]) : {}
   end
   helper_method :cart
 
   def enhanced_cart
+    
     @enhanced_cart ||= Product.where(id: cart.keys).map {|product| { product:product, quantity: cart[product.id.to_s] } }
   end
   helper_method :enhanced_cart
